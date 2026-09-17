1fa72d389c1ce37bf3aefa3ba70dfdc998fafbd7
chandrapant <chandrapant@gmail.com>
2026-09-17 13:47:13 +0530

    deploye project

---
+++ src/main/java/com/example/hellowelcome/HelloWelcomeApplication.java
@@ -0,0 +1,12 @@
+package com.example.hellowelcome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWelcomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWelcomeApplication.class, args);
+    }
+}
